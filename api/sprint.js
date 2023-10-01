304378516cdd3dde607e629833d3d60cbd4aac90
mike.sda@hotmail.com <mike.sda@hotmail.com>
2023-10-01 00:01:21 -0300

    correcting header

--- api/sprint.js
+++ api/sprint.js
@@ -1,6 +1,6 @@
 module.exports = async (req, res) => {
    res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Mehots', 'OPTIONS POST');
+   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS POST');
    res.setHeader('Content-Type', 'application/json');
    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
